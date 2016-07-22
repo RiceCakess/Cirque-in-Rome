b101b06789ac9657c597290b1536e6a70bae746e
RiceCakess <juandavidchen@gmail.com>
2016-07-22 09:31:40 -0400

    fixed collision

--- Assets/Scripts/chariotMovement.cs
+++ Assets/Scripts/chariotMovement.cs
@@ -8,10 +8,6 @@ public class chariotMovement : MonoBehaviour {
 	float currentSpeed = 0;
 	float accel = .02f;
 	float deccel = .05f;
-	int cameraBump = 60;
-	int length = 10;
-	float cameraRotation = 0;
-	int rotationAmt = 5;
 	Vector3 dirVector = new Vector3(0,0,0);
 	// Use this for initialization
 	GameObject camera;
@@ -28,33 +24,14 @@ public class chariotMovement : MonoBehaviour {
 
 	void OnCollisionEnter(Collision col){
 		if (col.gameObject.name == "otherChariot" || col.gameObject.name == "circus") {
-			col.gameObject.GetComponent<Rigidbody> ().AddForce (Vector3.back * 200f);
-			Debug.Log ("Test");
+			col.gameObject.GetComponent<Rigidbody> ().AddForce (-col.gameObject.GetComponent<Rigidbody>().transform.right * 200f);
+			Debug.Log("test");
 		}
 	}
 
 	void checkInput(){
-		//rotates camera based off of horizontal movement
 		transform.Rotate (new Vector3 (0, Input.GetAxis ("Mouse X"), 0) * Time.deltaTime * speed);
-		//uses translate to move relative to the camera angle ^ 
-		//speed is the speed of the chariot - change based off of stamina? 
-		cameraBump--;
-		if (cameraBump <= 0 && currentSpeed > 1) {
-			transform.GetChild (0).transform.Rotate (rotationAmt, 0, 0);
-			cameraRotation = -rotationAmt;
-			length = 10;
-			cameraBump = 200 - (int)currentSpeed *20;
-			if (cameraBump  < 30) {
-				cameraBump  = 30;
-			}
-		}
-		if (cameraRotation < 0) {
-			length--;
-			camera.transform.rotation = Quaternion.identity;
-			if (length <= 0) {
-				cameraRotation = 0;
-			}
-		}
+
 		if (Input.GetMouseButton (0)) {
 			accel = .5f;
 		} else {
@@ -72,7 +49,7 @@ public class chariotMovement : MonoBehaviour {
 		} else {
 			desiredSpeed = 0;
 		}
-		Debug.Log (desiredSpeed + " " + currentSpeed + " " + accel);
+		//x	Debug.Log (desiredSpeed + " " + currentSpeed + " " + accel);
 		if (currentSpeed < desiredSpeed) {
 			currentSpeed += accel;
 		} else if (currentSpeed > desiredSpeed) {
