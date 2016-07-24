b6dd4ad5e8dcf097f38c842367cb5dfd65dc0e15
aliyuanh <aliyuanh@yahoo.com>
2016-07-24 17:46:22 -0400

    added functional health and stamina (UI) and a blank end screen

--- Assets/Scripts/chariotMovement.cs
+++ Assets/Scripts/chariotMovement.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class chariotMovement : MonoBehaviour {
 	public float movementSpeed = 10f;
+	public float stamina = 20f;
+	public float health = 100;
 	float speed = 100.0f;
 	float desiredSpeed = 0;
 	float currentSpeed = 0;
@@ -12,18 +15,26 @@ public class chariotMovement : MonoBehaviour {
 	// Use this for initialization
 	GameObject camera;
 	void Start () {
+		StartCoroutine (rotateWheels ());
 		camera = transform.GetChild(0).transform.gameObject;
 		StartCoroutine (rotateCam ());
 		soundManager.instance.playBgm (soundManager.instance.bgm);
 	}
-	
+	IEnumerator rotateWheels(){
+		GetComponent<Animation> ().Play ("rotate");
+		yield return new WaitForSeconds (GetComponent<Animation> ()["rotate"].length);
+		StartCoroutine (rotateWheels ());
+
+	}
 	// Update is called once per frame
 	void Update () {
 		checkInput ();
 		if (Input.GetKey (KeyCode.Space)) {
 			Cursor.visible = !Cursor.visible;
 		}
-
+		if (health < 0) {
+			SceneManager.LoadScene (2);
+		}
 
 	}
 	IEnumerator rotateCam(){
@@ -45,9 +56,13 @@ public class chariotMovement : MonoBehaviour {
 
 
 	void OnCollisionEnter(Collision col){
-		if (col.gameObject.name == "otherChariot" || col.gameObject.name == "circus") {
+		if (col.gameObject.tag == "otherChariot" || col.gameObject.tag == "circus") {
 			col.gameObject.GetComponent<Rigidbody> ().AddForce (-col.gameObject.GetComponent<Rigidbody>().transform.right * 200f);
 			Debug.Log("test");
+			GameObject healthImage = GameObject.FindWithTag ("health");
+			Image heal = healthImage.GetComponent<Image> ();
+			heal.GetComponent<healthBar> ().hit ();
+			health -= 10;
 
 		}
 		if (col.gameObject.tag == "circus") {
@@ -59,7 +74,12 @@ public class chariotMovement : MonoBehaviour {
 	void checkInput(){
 		transform.Rotate (new Vector3 (0, Input.GetAxis ("Mouse X"), 0) * Time.deltaTime * speed);
 
-		if (Input.GetMouseButton (0)) {
+		if (Input.GetMouseButtonUp (0) && stamina >= 0) {
+			GameObject bar = GameObject.FindWithTag ("stamina");
+			Image health = bar.GetComponent<Image> ();
+			health.GetComponent<healthBar> ().hitStamina ();
+			print ("hit");
+			stamina--;
 			accel = .5f;
 		} else {
 			accel = .02f;
---
+++ Assets/Scripts/healthBar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class healthBar : MonoBehaviour {
+	Image bar;
+	// Use this for initialization
+	void Start () {
+		bar = GetComponent<Image> ();
+		bar.type = Image.Type.Filled;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void hitStamina(){
+		bar.fillAmount -= 1f/20f;
+	}
+	public void hit(){
+		bar.fillAmount -= .1f;
+	}
+}
