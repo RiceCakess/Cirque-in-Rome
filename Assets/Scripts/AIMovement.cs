441cdb2471337ed08e4f1f8aca77e60a871b9ad1
RiceCakess <juandavidchen@gmail.com>
2016-07-26 15:44:50 -0400

    AI Improvements

--- Assets/Scripts/AIMovement.cs
+++ Assets/Scripts/AIMovement.cs
@@ -13,25 +13,18 @@ public class AIMovement : MonoBehaviour {
 		navMeshAgent.SetDestination(waypoint.position);
 		chariotObjects = GameObject.FindGameObjectsWithTag ("otherChariot");
 	}
-	bool walking = false;
 	Transform waypoint;
 	KeyCode currentInput;
 	KeyCode[] codes = { KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.Q };
 	NavMeshAgent navMeshAgent;
 	GameObject[] chariotObjects;
 	int index = 1;
-	int frames = 0;
+	Vector3 lastPosition;
+	int notMoving = 0;
+	int time = 0;
+	public bool debug = false;
 	// Update is called once per frame
 	void Update () {
-		//navMeshAgent.Resume();
-		//control.sendInput (KeyCode.W);
-		/*if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance) {
-			if (!navMeshAgent.hasPath || Mathf.Abs (navMeshAgent.velocity.sqrMagnitude) < float.Epsilon) {
-				navMeshAgent.SetDestination(GameObject.Find ("waypoint4").transform.position);
-			}
-		} else {
-			walking = true;
-		}*/
 
 		foreach (GameObject g in chariotObjects) {
 			if (getDistance (g.transform, transform) < .01f) {
@@ -46,18 +39,33 @@ public class AIMovement : MonoBehaviour {
 			control.sendInput (KeyCode.W);
 		else
 			canMove = true;
-		if (getDistance (waypoint.transform, transform) <= .1f) {
-			//Debug.Log (index +" " +  getDistance (waypoint.transform, transform));
+		if (getDistance (waypoint.transform, transform) <= .5f) {
 			index++;
 			if (index > 4)
-				index = 1;			
+				index = 1;
+			waypoint = GameObject.Find ("waypoint"+index).transform;
+			navMeshAgent.SetDestination(waypoint.position += new Vector3(Random.value, 0, Random.value));
 		}
-		waypoint = GameObject.Find ("waypoint"+index).transform;
-		navMeshAgent.SetDestination(waypoint.position);
 
+		/*if (lastPosition != null) {
+			if (getDistance (lastPosition, transform.position) < .009f) {
+				notMoving++;
+			} else if (notMoving > 3) {
+				notMoving = 0;
+			}
+			if(debug)
+				Debug.Log (getDistance (transform.position, lastPosition) + " " + notMoving);
+			lastPosition = transform.position;
+		} else {
+			lastPosition = transform.position;
+		}
+		if (notMoving > 8 && control.currentSpeed > 6f) {
+			transform.Rotate (new Vector3 (0, 180f, 0));
+			notMoving = 0;
+		}
+		time++;*/
 
 	}
-	int time = 0;
 	void goTo(Transform pos){
 		float step = 2f * Time.deltaTime;
 		Vector3 target = pos.position - transform.position;
@@ -70,6 +78,12 @@ public class AIMovement : MonoBehaviour {
 	float getDistance(Transform d1, Transform d2){
 		float subx = (d1.position.x - d2.position.x);
 		float subz = (d2.position.z - d2.position.z);
+		float hyp = Mathf.Sqrt (subx * subx + subz * subz);
+		return hyp;
+	}
+	float getDistance(Vector3 d1, Vector3 d2){
+		float subx = (d1.x - d2.x);
+		float subz = (d2.z - d2.z);
 		float hyp = Mathf.Sqrt (subx * subx + subz * subz);
 		return hyp;
 	}
