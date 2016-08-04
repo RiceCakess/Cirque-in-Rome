8662f735ee262d4851b24b0f7b66d1b6a76d982c
aliyuanh <aliyuanh@yahoo.com>
2016-08-03 21:30:41 -0400

    added in the new chariots, made a hierarchy for them

---
+++ Assets/Scripts/charPlay.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class charPlay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (playChar ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	IEnumerator playChar(){
+		GetComponent<Animation> ().Play ("rotate");
+		yield return new WaitForSeconds (GetComponent<Animation> () ["rotate"].length - 1.5f);
+		StartCoroutine (playChar ());
+	}
+}
